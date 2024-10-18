96a8b25843380a4d108563452f54c7e8222adaf0
Wonnyfr <77871874+Wonnyfr@users.noreply.github.com>
2024-10-18 16:37:12 +1100

    a

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            finishEffect.Play();
+            Invoke("ReloadScene", loadDelay);    
         }   
+    }
+
+    void ReloadScene() 
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
